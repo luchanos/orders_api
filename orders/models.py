078d1bff70b282545ff7a46879ebe6385c7e8aae
user14881990 <712898@gmail.com>
2021-03-03 15:46:26 +0300

    Создал модель Orders(NewOrders),добавил данные,все функционирует.По типам полей надо додумать.!

--- orders/models.py
+++ orders/models.py
@@ -5,3 +5,15 @@ from django.db import models
 class DeviceType(models.Model):
     device_type = models.IntegerField()
     device_type_name = models.CharField(max_length=100)
+
+
+
+class NewOrders(models.Model):
+    status_order = models.IntegerField(default=0)
+    created_dt = models.DateTimeField(auto_now_add=True)
+    updated_dt = models.DateTimeField(auto_now=True)
+    uid = models.IntegerField(default=0)
+    description = models.TextField(blank=True)
+    customer_id = models.IntegerField(default=0)
+    user_id = models.IntegerField(default=0)
+    order_type = models.IntegerField(default=0)
